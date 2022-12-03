ac62767a74cecd8cfc866bd67584248c147abf5d
Niko <yourlocalgreek@duck.com>
2022-12-03 10:13:18 +0100

    solved the second part of the challenge

--- day_2/solution.py
+++ day_2/solution.py
@@ -2,7 +2,7 @@
 
 Author: Nick
 Version: 1.0
-Date: 2022-12-02
+Date: 2022-12-03
 
 """
 
@@ -12,14 +12,25 @@ with open("input.txt", "r", encoding="utf-8") as data:
         "".join(tuple(line.strip().replace(" ", ""))) for line in data.readlines()
     ]
 
-points_from_games = sum(
-    [6 for game in games_played if game in ["AY", "BZ", "CX"]]  # win 6p
+second_column = [moves[1] for moves in games_played]
+
+strategy_one_points = sum( # Strategy 1
+      [6 for game in games_played if game in ["AY", "BZ", "CX"]]  # win 6p
     + [3 for game in games_played if game in ["AX", "BY", "CZ"]]  # draw 3p
+    + [second_column.count("X") + second_column.count("Y") * 2 + second_column.count("Z") * 3]
 )
 
-my_moves = [my_move for (op_move, my_move) in games_played]
-points_from_moves = sum(  # rock 1p | paper 2p | scissors 3p
-    [my_moves.count("X") + my_moves.count("Y") * 2 + my_moves.count("Z") * 3]
+strategy_two_points = sum( # Strategy 2
+      [1 for game in games_played if game == "BX"]
+    + [2 for game in games_played if game == "CX"]
+    + [3 for game in games_played if game == "AX"]
+    + [4 for game in games_played if game == "AY"]
+    + [5 for game in games_played if game == "BY"]
+    + [6 for game in games_played if game == "CY"]
+    + [7 for game in games_played if game == "CZ"]
+    + [8 for game in games_played if game == "AZ"]
+    + [9 for game in games_played if game == "BZ"]
+
 )
 
-print(f"Total points: {points_from_moves + points_from_games}")
+print(f"Strategy one: {strategy_one_points} | Strategy two: {strategy_two_points}")
